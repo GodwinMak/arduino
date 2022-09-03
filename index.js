53f88dd2d94577b071912b9d5fb4870e002ef787
GodwinMaKyao <godwinnamwel@gmail.com>
2022-09-03 13:43:19 +0300

    fix: refactor hard coded connection string to Heroku config var

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 
 const bodyParser = require("body-parser");
-
+const cors = require('cors');
 
 
 const dataRoutes = require('./Routes/dataRoute')
@@ -14,6 +14,8 @@ require('dotenv').config({ path: './.env' });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
+
 
 
 app.use('/api/v1',dataRoutes);
